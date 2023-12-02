386d11ac3dcfb73917ecf9bc1befa31bf157e629
Vitaliya27 <vitality.prohorenko@gmail.com>
2023-12-03 01:22:30 +0200

    Ready task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const showColor = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+function onChangeColor() {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  showColor.textContent = bgColor;
+
+  console.log(showColor.textContent);
+}
+
+changeColorButton.addEventListener("click", onChangeColor);
